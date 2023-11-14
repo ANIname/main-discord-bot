c3746c70561688fb79d48cbd27e10387ad765f89
kiidii <kiidii@aniname.com>
2023-11-14 20:31:05 +0200

    Update infrastructure

--- constants/path.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-
-exports.ROOT_DIRECTORY_PATH             = path.resolve(__dirname, '..');
-exports.SERVICES_DIRECTORY_PATH         = path.join(exports.ROOT_DIRECTORY_PATH, 'services');
-exports.NODE_MODULES_DIRECTORY_PATH     = path.join(exports.ROOT_DIRECTORY_PATH, 'node_modules');
-exports.WEBPACK_OUTPUT_DIRECTORY_PATH   = path.join(exports.ROOT_DIRECTORY_PATH, '.webpack-output');
-exports.LAMBDA_FUNCTIONS_DIRECTORY_PATH = path.join(exports.ROOT_DIRECTORY_PATH, 'lambda-functions');
