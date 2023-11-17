301d22f006bd1fe28590bb6d56ab98f80b1b9118
kiidii <kiidii@aniname.com>
2023-11-17 11:38:17 +0200

    code cleanUp

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,16 +1,6 @@
 import { directoryImport } from 'directory-import'
-import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js'
 
-export type CommandName = string
-
-export interface Command {
-  data: SlashCommandBuilder
-  execute: (interaction: ChatInputCommandInteraction) => Promise<void>
-}
-
-export interface Commands {
-  [key: CommandName]: Command
-}
+import { Command, Commands } from './types.d'
 
 const commands: Commands = {}
 
