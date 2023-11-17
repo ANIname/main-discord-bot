b47810c685371177f2415393a4dd6021120542d7
kiidii <kiidii@aniname.com>
2023-11-17 22:08:30 +0200

    code cleanUp

--- src/client.ts
+++ src/client.ts
@@ -1,24 +1,8 @@
-import { directoryImport } from 'directory-import'
-import { Client, ClientEvents } from 'discord.js'
-import camelCase from 'lodash/camelCase'
+import { Client } from 'discord.js'
 
 import getAllIntents from '../utils/discord/get-all-intents'
-import { EventData } from './types.d'
-
 const { DISCORD_BOT_TOKEN } = process.env
 
 export const client = new Client({ intents: getAllIntents() })
 
 client.login(DISCORD_BOT_TOKEN)
-
-// Import all events from the events directory and add them to the client
-// Example: ./events/ready.ts => client.on('ready', () => { ... })
-// Example: ./events/message-create.ts => client.on('messageCreate', () => { ... })
-directoryImport('./events', (moduleName, _, moduleData) => {
-  const eventName = camelCase(moduleName) as keyof ClientEvents
-  const listener = (moduleData as EventData).default
-
-  if (typeof listener !== 'function') return
-
-  client.on(eventName, (...arguments_) => listener(client, ...arguments_))
-})
