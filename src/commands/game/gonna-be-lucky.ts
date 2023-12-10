0045494dc75593c820c0d5bf43721035027597f3
kiidii <kiidii@aniname.com>
2023-12-10 22:31:27 +0200

    added gonna-be-lucky game

---
+++ src/commands/game/gonna-be-lucky.ts
@@ -0,0 +1,149 @@
+import { ChatInputCommandInteraction, SlashCommandBuilder } from 'discord.js'
+import fetch from 'node-fetch'
+
+import openAi from '../../../services/open-ai'
+
+const { BACKEND_URL } = process.env
+
+interface User {
+  _id: string
+  discord: {
+    id: string
+  }
+  games?: {
+    gonnaBeLucky?: {
+      points: number
+      events: GameEvent[]
+    }
+  }
+}
+
+interface GameEvent {
+  event: string
+  points: number
+  increase: boolean
+  declination: string
+  date?: Date
+}
+
+interface GameTimeOut {
+  [key: string]: Date
+}
+
+const timeOut = 1000 * 60 * 60 // 1 hour
+
+const gameTimeOut: GameTimeOut = {}
+
+export const data = new SlashCommandBuilder()
+  .setName('gonna-be-lucky')
+  .setDescription('Generates a random event and gives or takes away points')
+
+const promt = (mention: string) =>
+  'Мы играем в игру:"gonna be lucky".' +
+  '\n Суть игры проста. Ты генерируешь случайное событие (забавное, смешное, или обычное) и даёшь или отнимаешь очки.' +
+  `\n Например: "${mention} решил прогуляться и потерял кошелёк. -10 очков.".` +
+  `\n Или: "${mention} пошел в казино и выбил джекпот. +100 очков."` +
+  `\n Или: "${mention} не смог найти работу. -50 очков."` +
+  '\n Я буду считать очки и вести таблицу лидеров.' +
+  '\n От тебя я хочу только сгенерированное событие и количество очков. Не больше, не меньше.' +
+  '\n Формат json объект. Где event - это событие, points - это очки, increase - тут ты указываешь булеаном добавляешь очки, или отнимаешь, declination - указываешь склонение (очко, очка, очков)' +
+  '\n Важно чтобы формат был именно такой, иначе я не смогу правильно посчитать очки.' +
+  '\n Важно чтобы ты не указывал количество очков в поле event или declination. Указывай очки только в поле points.' +
+  `\n Пример: {"event": "${mention} пошел в казино и выбил джекпот.", "points": 100, "increase": true, "declination": "очков"}` +
+  '\n Буду рад если ты будешь генерировать забавные события, суть в том чтобы было весело, а так-же не будешь повторяться.' +
+  '\n Не нужно оборачивать сообщение дополнительно в кавычки. Тоесть вместо \'"текст"\' пиши просто \'текст\'.' +
+  '\n Мне нужно только одно событие' +
+  `\n Где: ${mention} - никнейм игрока.`
+
+/**
+ * Generates a random event and gives or takes away points
+ * @param {ChatInputCommandInteraction} interaction - Discord Interaction
+ */
+export async function execute(interaction: ChatInputCommandInteraction) {
+  const time = gameTimeOut[interaction.user.id]
+
+  if (time && Date.now() - time.getTime() < timeOut) {
+    await interaction.reply({
+      content: `Пожалуйста, наберитесь терпения! Подождите еще ${Math.floor((timeOut - (Date.now() - time.getTime())) / 1000 / 60)} мин. И я снова буду готова!`,
+      ephemeral: true
+    })
+    return
+  }
+
+  await interaction.deferReply()
+
+  const result = await openAi.chat.completions.create({
+    model: 'gpt-4',
+    messages: [
+      { role: 'system', content: promt(`<@${interaction.user.id}>`) },
+    ]
+  })
+
+  const { event, points, increase, declination } = JSON.parse(result.choices[0]?.message.content || '{}') as GameEvent
+
+  const message = `${event} ${increase ? '+' : '-'}${points} ${declination}`
+
+  await interaction.editReply({ content: message })
+
+  console.log({
+    event, points, increase, declination 
+  })
+
+  const getUserResponse = await fetch(`${BACKEND_URL}/user?discord={ "id": ${interaction.user.id} }`)
+  const user = await getUserResponse.json() as User
+  
+  const updateUserResponse = await fetch(`${BACKEND_URL}/user?discord={ "id": ${interaction.user.id} }&options={ "upsert": true }`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      discord: {
+        id: interaction.user.id
+      },
+
+      games: {
+        gonnaBeLucky: {
+          points: getPoints(user, points, increase),
+          events: getEvents(user, event, points, increase, declination)
+        }
+      }
+    })
+  })
+
+  console.log(await updateUserResponse.json())
+
+  gameTimeOut[interaction.user.id] = new Date()
+}
+
+/**
+ * @param {User} user - User
+ * @param {number} pointsToSet - Points to set
+ * @param {boolean} increase - Increase or decrease points
+ * @returns {number} - Points
+ */
+function getPoints(user: User, pointsToSet: number, increase: boolean): number {
+  const points = (user?.games?.gonnaBeLucky?.points || 0) as number
+
+  return increase ? points + pointsToSet : points - pointsToSet
+}
+
+/**
+ * @param {User} user - User
+ * @param {string} event - Event
+ * @param {number} points - Points
+ * @param {boolean} increase - Increase or decrease points
+ * @param {string} declination - Declination
+ * @returns {GameEvent[]} - Events
+ */
+function getEvents(user: User, event: string, points: number, increase: boolean, declination: string): GameEvent[] {
+  const events = (user?.games?.gonnaBeLucky?.events || []) as GameEvent[]
+
+  return [...events, {
+    event,
+    points,
+    increase,
+    declination,
+    date: new Date()
+  }]
+}
