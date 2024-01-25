579b032bdb7e5ca55441a0b1977683cd0f836280
kiidii <kiidii@aniname.com>
2024-01-25 21:08:11 +0200

    added comy .md files command

--- utils/discord/update-welcome-channel/messages-contents/index.ts
+++ utils/discord/update-welcome-channel/messages-contents/index.ts
@@ -2,21 +2,21 @@ import path from 'path'
 import { readFileSync } from 'fs'
 
 const messagesContentsEn = [
-  { content: readFileSync(path.join(__dirname, '../../../../utils/discord/update-welcome-channel/messages-contents', 'first-message-en.md'), 'utf-8') },
-  { content: readFileSync(path.join(__dirname, '../../../../utils/discord/update-welcome-channel/messages-contents', 'second-message-en.md'), 'utf-8') },
-  { content: readFileSync(path.join(__dirname, '../../../../utils/discord/update-welcome-channel/messages-contents', 'third-message-en.md'), 'utf-8') },
+  { content: readFileSync(path.join(__dirname, 'first-message-en.md'), 'utf-8') },
+  { content: readFileSync(path.join(__dirname, 'second-message-en.md'), 'utf-8') },
+  { content: readFileSync(path.join(__dirname, 'third-message-en.md'), 'utf-8') },
 ]
 
 const messagesContentsUa = [
-  { content: readFileSync(path.join(__dirname, '../../../../utils/discord/update-welcome-channel/messages-contents', 'first-message-ua.md'), 'utf-8') },
-  { content: readFileSync(path.join(__dirname, '../../../../utils/discord/update-welcome-channel/messages-contents', 'second-message-ua.md'), 'utf-8') },
-  { content: readFileSync(path.join(__dirname, '../../../../utils/discord/update-welcome-channel/messages-contents', 'third-message-ua.md'), 'utf-8') },
+  { content: readFileSync(path.join(__dirname, 'first-message-ua.md'), 'utf-8') },
+  { content: readFileSync(path.join(__dirname, 'second-message-ua.md'), 'utf-8') },
+  { content: readFileSync(path.join(__dirname, 'third-message-ua.md'), 'utf-8') },
 ]
 
 const messagesContentsRu = [
-  { content: readFileSync(path.join(__dirname, '../../../../utils/discord/update-welcome-channel/messages-contents', 'first-message-ru.md'), 'utf-8') },
-  { content: readFileSync(path.join(__dirname, '../../../../utils/discord/update-welcome-channel/messages-contents', 'second-message-ru.md'), 'utf-8') },
-  { content: readFileSync(path.join(__dirname, '../../../../utils/discord/update-welcome-channel/messages-contents', 'third-message-ru.md'), 'utf-8') },
+  { content: readFileSync(path.join(__dirname, 'first-message-ru.md'), 'utf-8') },
+  { content: readFileSync(path.join(__dirname, 'second-message-ru.md'), 'utf-8') },
+  { content: readFileSync(path.join(__dirname, 'third-message-ru.md'), 'utf-8') },
 ]
 
 export default {
