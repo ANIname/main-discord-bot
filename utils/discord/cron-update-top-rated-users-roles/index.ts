4016bdf3c2aca13d660dc95b7391d816d56b7a85
kiidii <kiidii@aniname.com>
2024-01-27 13:48:27 +0200

    deleted update-role-color handler

--- utils/discord/cron-update-top-rated-users-roles/index.ts
+++ utils/discord/cron-update-top-rated-users-roles/index.ts
@@ -1,4 +1,3 @@
-import chromaJs from 'chroma-js'
 import { CronJob } from 'cron'
 import declineWord from 'decline-word'
 import { Guild, Snowflake } from 'discord.js'
@@ -9,12 +8,8 @@ import deleteMembersFromUnusedRole from './delete-members-from-unused-role'
 import deleteRoleMembersIfLengthIsMoreThanOne from './delete-role-members-if-length-is-more-than-one'
 import updateRoleMember from './update-role-member'
 import updateRoleName from './update-role-name'
-import updateRoleColor from './update-role-color'
-
 import { getUsersRating } from '../../../services/knex/base-queries/user'
 import { UserRating } from '../../../services/knex/types.d'
-
-const gradient = chromaJs.scale(['#86FF99', '#FFD166', '#88C0FC', '#BE9EFF', '#FFB8E0']).domain([100, 80, 50, 20, 1])
 
 let isLoopRunning = false
 let tryings = 0
@@ -47,11 +42,9 @@ export default async (guild: Guild) => new CronJob('* * * * * *', async () => {
     const totalPoints = userRating?.totalPoints || 0
 
     const newRoleName   = `Top Rated User #${roleNumber} (${totalPoints} ${declineWord(totalPoints, 'point', '' , 's', 's')})`
-    const newRoleColor  = Number(gradient(roleNumber).num().toFixed(0))
     const newRoleMember = (userRating) ? guild.members.cache.get(userRating.discordId as Snowflake) : undefined
 
     const currentRoleName    = role.name
-    const currentRoleColor   = role.color
     const currentRoleMember  = role.members.first()
     const currentRoleMembers = role.members.map((member) => member)
 
@@ -59,7 +52,6 @@ export default async (guild: Guild) => new CronJob('* * * * * *', async () => {
     await deleteRoleMembersIfLengthIsMoreThanOne(currentRoleMembers, role)
     await updateRoleMember(currentRoleMember, newRoleMember, role)
     await updateRoleName(currentRoleName, newRoleName, role)
-    await updateRoleColor(currentRoleColor, newRoleColor, role)
   }
 
   isLoopRunning = false
