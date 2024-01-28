de7ea25da335272f2e68addadcfafbf0cf2a2d9d
kiidii <kiidii@aniname.com>
2024-01-28 21:22:47 +0200

    updated error message

--- src/commands/game/gonna-be-lucky/generate-event.ts
+++ src/commands/game/gonna-be-lucky/generate-event.ts
@@ -23,7 +23,11 @@ export default async function generateEvent(interaction: ChatInputCommandInterac
     if (error instanceof SyntaxError) {
       const errorsChannel = interaction.client.channels.cache.get('1200763445474238524') as TextChannel
 
-      await errorsChannel.send(`Ошибка при парсинге JSON объекта. Событие не было добавлено в игру. Вот что сгенерировал бот:\n${result.choices[0]?.message.content}`)
+      await errorsChannel.send(
+        'Ошибка при парсинге JSON объекта. Событие не было добавлено в игру.' +
+        `\nВот что сгенерировал бот:\n${result.choices[0]?.message.content}.` +
+        `\nТекст ошибки: ${error}`
+      )
 
       return generateEvent(interaction)
     }
