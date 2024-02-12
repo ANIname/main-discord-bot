7d1620793c2047c9c99583458f93f5d924b468ca
kiidii <kiidii@aniname.com>
2024-02-12 18:40:24 +0200

    fixed bug with deferReply

--- src/commands/bot/update-commands/index.ts
+++ src/commands/bot/update-commands/index.ts
@@ -9,14 +9,17 @@ export const commandName = 'update-commands'
 /**
  * Update all discord bot commands
  * @param {ChatInputCommandInteraction} interaction - Represents a command interaction
- * @returns {Promise<unknown>} - Promise with unknown result
  */
-export async function execute (interaction: ChatInputCommandInteraction): Promise<unknown> {
+export async function execute (interaction: ChatInputCommandInteraction) {
+  await interaction.deferReply({ ephemeral: true })
+
   const commands = await getCommands()
 
   const rest = new REST({ version: '10' }).setToken(DISCORD_BOT_TOKEN as string)
 
   const routeApplicationCommands = Routes.applicationCommands(interaction.client.user.id)
 
-  return rest.put(routeApplicationCommands, { body: commands })
+  await rest.put(routeApplicationCommands, { body: commands })
+
+  await interaction.editReply('Commands updated')
 }
