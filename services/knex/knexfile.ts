98f9670298c2734eb1a8d4bef38b1818768030ee
KiiDii <kiidii.aniname@gmail.com>
2025-07-18 19:27:49 +0300

    🔧 REFACTOR: enhance PostgreSQL SSL configuration by adding dynamic retrieval of SSL certificates

--- services/knex/knexfile.ts
+++ services/knex/knexfile.ts
@@ -1,16 +1,39 @@
+import { execSync } from 'node:child_process'
+
 const { SERVER_HOST, POSTGRESQL_PORT, POSTGRESQL_USERNAME, POSTGRESQL_PASSWORD, POSTGRESQL_DATABASE } = process.env
 
 const knexConfig = {
   client: 'pg',
+
   connection: {
+    ssl: getPostgreSQL_SSLConfig(),
+
     host: SERVER_HOST,
     port: Number(POSTGRESQL_PORT),
+
     user: POSTGRESQL_USERNAME,
     password: POSTGRESQL_PASSWORD,
     database: POSTGRESQL_DATABASE,
-    ssl: true
   },
+
   pool: { min: 1, max: 10 },
+}
+
+function getPostgreSQL_SSLConfig() {
+  console.time('⚙️  Get PostgreSQL SSL config')
+
+  const command = 'openssl s_client'
+    + ' -showcerts'
+    + ` -connect ${SERVER_HOST}:${POSTGRESQL_PORT}`
+    + ' -starttls postgres'
+    + ' < /dev/null 2>/dev/null'
+    + ' | sed -ne \'/^-----BEGIN CERTIFICATE-----\$/,/^-----END CERTIFICATE-----\$/p\''
+
+  const caBuffer = execSync(command)
+
+  console.timeEnd('⚙️  Get PostgreSQL SSL config')
+
+  return { ca: caBuffer, rejectUnauthorized: false }
 }
 
 export default knexConfig;
