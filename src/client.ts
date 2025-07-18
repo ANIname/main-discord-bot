eaeb57bb9f2eaf311b01e618bca6c81eace393b9
KiiDii <kiidii.aniname@gmail.com>
2025-07-18 19:29:58 +0300

    🔧 REFACTOR: remove NODE_TLS_REJECT_UNAUTHORIZED

--- src/client.ts
+++ src/client.ts
@@ -2,8 +2,6 @@ import { Client } from 'discord.js'
 
 import getAllIntents from '../utils/discord/get-all-intents'
 
-process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0'
-
 const { DISCORD_BOT_TOKEN } = process.env
 
 export const client = new Client({ intents: getAllIntents() })
